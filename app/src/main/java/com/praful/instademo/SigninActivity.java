a1ac066a560bf2f1e2c964ba3313dc89752bd206
Praful Dhabekar <pddhabekar@gmail.com>
2017-07-31 01:22:37 +0530

    SignupActivity added

--- app/src/main/java/com/praful/instademo/SigninActivity.java
+++ app/src/main/java/com/praful/instademo/SigninActivity.java
@@ -1,7 +1,10 @@
 package com.praful.instademo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SigninActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        Button signup = (Button)findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SigninActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
